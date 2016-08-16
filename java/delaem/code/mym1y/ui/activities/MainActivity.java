8015a9a6ea77f865a89dbff98277a78c24be54d5
kepocnhh <kepocnhh@gmail.com>
2016-08-16 21:46:56 +0300

    create project

---
+++ java/delaem/code/mym1y/ui/activities/MainActivity.java
@@ -0,0 +1,27 @@
+package delaem.code.mym1y.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import delaem.code.mym1y.R;
+
+public class MainActivity
+        extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        initViews();
+        init();
+    }
+    private void initViews()
+    {
+
+    }
+    private void init()
+    {
+
+    }
+}
